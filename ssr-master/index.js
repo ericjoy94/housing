b5fe253709c627a84a174c1a2d3c452848ccda80
Eric Joy <eric.joy@mail.vinove.com>
2018-07-18 12:52:33 +0530

    https/SSL protocols added

--- ssr-master/index.js
+++ ssr-master/index.js
@@ -1,8 +1,10 @@
 const express = require('express'),
           app = express(),
      template = require('./views/template')
-         path = require('path');
-
+         path = require('path'),
+         spdy = require('spdy'),
+         fs = require('fs'),
+         port = 3000
 
 // Serving static files
 app.use('/assets', express.static(path.resolve(__dirname, 'assets')));
@@ -11,7 +13,7 @@ app.use('/media', express.static(path.resolve(__dirname, 'media')));
 // hide powered by express
 app.disable('x-powered-by');
 // start the server
-app.listen(process.env.PORT || 3000);
+// app.listen(process.env.PORT || 3000);
 
 // our apps data model
 const data = require('./assets/data.json');
@@ -24,8 +26,14 @@ let initialState = {
 //SSR function import
 const ssr = require('./views/server');
 
+const options = {
+  key: fs.readFileSync(__dirname + '/http2-express/server.key'),
+  cert:  fs.readFileSync(__dirname + '/http2-express/server.crt')
+}
+
 // server rendered home page
 app.get('/', (req, res) => {
+ 
   const { preloadedState, content}  = ssr(initialState)
   const response = template("Server Rendered Page", preloadedState, content)
   res.setHeader('Cache-Control', 'assets, max-age=604800')
@@ -50,3 +58,14 @@ app.get('/client', (req, res) => {
     }
 
   });
+
+  spdy
+  .createServer(options, app)
+  .listen(port, (error) => {
+    if (error) {
+      console.error(error)
+      return process.exit(1)
+    } else {
+      console.log('Listening on port: ' + port + '.')
+    }
+  })
